783aee0d788bbf401eaf731871eed60a5beb1339
Mark Carrington <MarkMpn@users.noreply.github.com>
2021-03-26 18:55:57 +0000

    Add project files.

---
+++ MarkMpn.CustomActionToApiConverter/PluginControl.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace MarkMpn.CustomActionToApiConverter
+{
+    partial class PluginControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.solutionComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.solutionPanel = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.customActionListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.propertyGrid = new System.Windows.Forms.PropertyGrid();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.solutionPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // solutionComboBox
+            // 
+            this.solutionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.solutionComboBox.FormattingEnabled = true;
+            this.solutionComboBox.Location = new System.Drawing.Point(57, 3);
+            this.solutionComboBox.Name = "solutionComboBox";
+            this.solutionComboBox.Size = new System.Drawing.Size(345, 21);
+            this.solutionComboBox.TabIndex = 0;
+            this.solutionComboBox.SelectedIndexChanged += new System.EventHandler(this.solutionComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Solution:";
+            // 
+            // solutionPanel
+            // 
+            this.solutionPanel.Controls.Add(this.label1);
+            this.solutionPanel.Controls.Add(this.solutionComboBox);
+            this.solutionPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.solutionPanel.Location = new System.Drawing.Point(0, 0);
+            this.solutionPanel.Name = "solutionPanel";
+            this.solutionPanel.Size = new System.Drawing.Size(853, 28);
+            this.solutionPanel.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 28);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.customActionListView);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.propertyGrid);
+            this.splitContainer1.Size = new System.Drawing.Size(853, 615);
+            this.splitContainer1.SplitterDistance = 525;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(4);
+            this.panel1.Size = new System.Drawing.Size(525, 34);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(4, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(517, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Custom Actions";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // customActionListView
+            // 
+            this.customActionListView.CheckBoxes = true;
+            this.customActionListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.customActionListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.customActionListView.FullRowSelect = true;
+            this.customActionListView.HideSelection = false;
+            this.customActionListView.Location = new System.Drawing.Point(0, 34);
+            this.customActionListView.Name = "customActionListView";
+            this.customActionListView.Size = new System.Drawing.Size(525, 581);
+            this.customActionListView.TabIndex = 2;
+            this.customActionListView.UseCompatibleStateImageBehavior = false;
+            this.customActionListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Custom Action Name";
+            this.columnHeader1.Width = 159;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(446, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // propertyGrid
+            // 
+            this.propertyGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyGrid.Location = new System.Drawing.Point(0, 0);
+            this.propertyGrid.Name = "propertyGrid";
+            this.propertyGrid.Size = new System.Drawing.Size(324, 615);
+            this.propertyGrid.TabIndex = 0;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Message Name";
+            this.columnHeader2.Width = 162;
+            // 
+            // PluginControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.solutionPanel);
+            this.Name = "PluginControl";
+            this.Size = new System.Drawing.Size(853, 643);
+            this.solutionPanel.ResumeLayout(false);
+            this.solutionPanel.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox solutionComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel solutionPanel;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView customActionListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PropertyGrid propertyGrid;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
---
+++ MarkMpn.CustomActionToApiConverter/PluginControl.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using XrmToolBox.Extensibility;
+using XrmToolBox.Extensibility.Interfaces;
+
+namespace MarkMpn.CustomActionToApiConverter
+{
+    public partial class PluginControl : PluginControlBase, IGitHubPlugin, IHelpPlugin, IPayPalPlugin
+    {
+        public PluginControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnConnectionUpdated(ConnectionUpdatedEventArgs e)
+        {
+            base.OnConnectionUpdated(e);
+
+            LoadSolutions();
+        }
+
+        private void LoadSolutions()
+        {
+            solutionComboBox.Items.Clear();
+
+            if (ConnectionDetail == null)
+                return;
+
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Loading Solutions...",
+                Work = (bw, args) =>
+                {
+                    var qry = new QueryExpression("solution");
+                    qry.ColumnSet = new ColumnSet("solutionid", "friendlyname");
+                    var componentLink = qry.AddLink("solutioncomponent", "solutionid", "solutionid", JoinOperator.Exists);
+                    var workflowLink = componentLink.AddLink("workflow", "objectid", "workflowid");
+                    workflowLink.LinkCriteria.AddCondition("category", ConditionOperator.Equal, 3); // Action
+                    qry.AddOrder("friendlyname", OrderType.Ascending);
+
+                    var results = Service.RetrieveMultiple(qry);
+                    var solutions = results.Entities
+                        .Select(entity => new EntityReference
+                        {
+                            LogicalName = entity.LogicalName,
+                            Name = entity.GetAttributeValue<string>("friendlyname"),
+                            Id = entity.Id
+                        })
+                        .ToList();
+
+                    args.Result = solutions;
+                },
+                PostWorkCallBack = args =>
+                {
+                    var solutions = (List<EntityReference>)args.Result;
+                    solutionComboBox.DisplayMember = nameof(EntityReference.Name);
+                    solutionComboBox.ValueMember = nameof(EntityReference.Id);
+                    solutionComboBox.DataSource = solutions;
+                }
+            });
+        }
+
+        string IGitHubPlugin.UserName => "MarkMpn";
+
+        string IGitHubPlugin.RepositoryName => "CustomActionToApiConverter";
+
+        string IHelpPlugin.HelpUrl => "https://markcarrington.dev/custom-action-to-api-converter/";
+
+        string IPayPalPlugin.DonationDescription => "Custom Action to Custom API Converter Donation";
+
+        string IPayPalPlugin.EmailAccount => "donate@markcarrington.dev";
+
+        private void solutionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var solutionId = (Guid)solutionComboBox.SelectedValue;
+            customActionListView.Items.Clear();
+
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Loading Custom Actions...",
+                Work = (bw, args) =>
+                {
+                    var qry = new QueryExpression("workflow");
+                    qry.ColumnSet = new ColumnSet("name", "uniquename");
+                    qry.Criteria.AddCondition("category", ConditionOperator.Equal, 3);
+                    var componentLink = qry.AddLink("solutioncomponent", "workflowid", "objectid");
+                    componentLink.LinkCriteria.AddCondition("solutionid", ConditionOperator.Equal, solutionId);
+                    var sdkMessageLink = qry.AddLink("sdkmessage", "sdkmessageid", "sdkmessageid");
+                    sdkMessageLink.EntityAlias = "msg";
+                    sdkMessageLink.Columns = new ColumnSet("name");
+                    qry.AddOrder("name", OrderType.Ascending);
+
+                    var results = Service.RetrieveMultiple(qry);
+                    args.Result = results;
+                },
+                PostWorkCallBack = args =>
+                {
+                    var actions = (EntityCollection)args.Result;
+
+                    foreach (var action in actions.Entities)
+                    {
+                        var lvi = customActionListView.Items.Add(action.GetAttributeValue<string>("name"));
+                        lvi.SubItems.Add((string) action.GetAttributeValue<AliasedValue>("msg.name").Value);
+                        lvi.Tag = action;
+                        lvi.Checked = true;
+                    }
+                }
+            });
+        }
+    }
+}
